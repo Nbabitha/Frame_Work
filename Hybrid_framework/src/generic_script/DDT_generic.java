0e7c89d532311ab945a3893037dc7aa7252964da
DELL <DELL@DESKTOP-N59INAR>
2023-07-06 05:32:27 +0530

    Build B03

--- Hybrid_framework/src/generic_script/DDT_generic.java
+++ Hybrid_framework/src/generic_script/DDT_generic.java
@@ -14,11 +14,11 @@ public static String getData(String sh, int r, int c)
 	  String value=null;
 	  
 	  try {
-	  FileInputStream fis=new FileInputStream("./Project.xlsx");
-	  Workbook book=WorkbookFactory.create(fis);
-	  Sheet sheet=book.createSheet(sh);
-	  Row row=sheet.createRow(r);
-	  Cell cell=row.createCell(c);	
+	  FileInputStream fis=new FileInputStream("./Project.xlsx");//path is specified
+	  Workbook book=WorkbookFactory.create(fis);//workbook is created
+	  Sheet sheet=book.createSheet(sh);//created sheet
+	  Row row=sheet.createRow(r);//row is created
+	  Cell cell=row.createCell(c);	//cell is created
 	  value=cell.toString();
 	  }
 	  catch(Exception e)
